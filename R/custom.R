7c313c7697b45ed21dc1e1480c29767c2b18a8a5
Tanmay Sharma <tanmaysharma19@gmail.com>
2021-03-13 12:46:14 -0800

    Add skeleton for dash app in R

---
+++ R/custom.R
@@ -0,0 +1,53 @@
+#' List of Custom CSS Specs
+#'
+#' @return A named list of lists containing CSS specifications
+#' @export
+custom_css <- function() {
+  css <- list()
+  
+  # Input parameter box
+  css$box <- list(
+    "border" = "1px solid #d3d3d3",
+    "border-radius" = "10px",
+    "margin" = "0px",
+    "background-color" = "rgba(220, 220, 220, 0.5)"
+  )
+  
+  # Dashboard header parameters
+  css$header <- list(
+    "background-color" =  "gray",
+    "padding" = 20,
+    "color" = "white",
+    "margin-top" = 20,
+    "margin-bottom" = 20,
+    "font-size" = "48px",
+    "border-radius" = 3
+  )
+  
+  # Drop-down choices
+  css$dd <- list("font-size" = "smaller")
+  
+  # Footnote text
+  css$sources <- list("font-size" = "xx-small")
+  
+  css$no_left_pad <- list("margin-left" = "0px")
+  
+  # Plotly fonts
+  css$plotly <- list("family" = paste0(
+    "-apple-system,",
+    "BlinkMacSystemFont,",
+    "'Segoe UI',",
+    "Roboto,",
+    "'Helvetica Neue',",
+    "Arial"
+  ))
+  
+  # radio-buttons
+  css$radio_buttons <- list(
+    "margin-left" = "10px",
+    "margin-right" = "5px"
+  )
+  
+  # Return CSS
+  css
+}
